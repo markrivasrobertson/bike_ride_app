96fb79170f3d67b104d0aa956fc595566a7c625f
Mark Robertson <markrivasrobertson@yahoo.com>
2016-11-20 14:08:44 -0500

    set up server.js initially.  Without import for app.js, which is not yet written

---
+++ server.js
@@ -0,0 +1,30 @@
+const path = require('path');
+// const app
+const express = require('express');
+
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config.js');
+
+const compiler = webpack(config);
+const webpackMiddle = webpackDevMiddleware(compiler, {
+  publicPath: '/js',
+  stats: {
+    chunks: false,
+    colors: true,
+  },
+});
+
+app.use(webpackMiddle);
+app.use(webpackHotMiddleware(compiler));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Listening port ${port}`);
+});
